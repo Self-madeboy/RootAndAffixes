3a7d7b0cd7f00934ce52a9e52c538f4b95d1d150
jiangchao <jiangchao@matech.cn>
2022-05-23 16:05:26 +0800

    初次提交

---
+++ src/main/java/com/root/affixes/AffixesApplication.java
@@ -0,0 +1,13 @@
+package com.root.affixes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AffixesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AffixesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/root/affixes/AffixesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.root.affixes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AffixesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
